e80086a0981a9b48a8f3944a22c8b96e08f9ec4d
mohamed nagy <mohamed nagy>
2017-05-04 01:51:52 +0200

    Ready to deploy

--- src/com/crunchify/tutorials/DataHandler.java
+++ src/com/crunchify/tutorials/DataHandler.java
@@ -45,11 +45,13 @@ public class DataHandler {
 	static Logger logger = Logger.getLogger(MainService.class);
 	
 	public static boolean isJoinedBefore(String senderId){
+		printall(joinedBeforeIds);
+		System.out.println(senderId);
 		System.out.println("hi*//********************************************************************");
 		BasicDBObject whereQuery = new BasicDBObject();
 		  whereQuery.put("senderId", senderId);
 		  DBCursor cursor3 = joinedBeforeIds.find(whereQuery);
-		  System.out.println(cursor3.hasNext()+" "+cursor3.size());
+		  System.out.println("isJoinedBefore "+cursor3.hasNext()+" "+cursor3.size());
 		  return cursor3.hasNext();
 //		return joinedBefore.contains(senderId);
 	}
@@ -57,6 +59,8 @@ public class DataHandler {
 		BasicDBObject data = new BasicDBObject();
 		data.append("senderId", senderId);
 		joinedBeforeIds.insert(data);
+		printall(joinedBeforeIds);
+		
 	}
 	public static boolean isInChattingBool(String senderId) {
 		BasicDBObject whereQuery = new BasicDBObject();
@@ -70,7 +74,7 @@ public class DataHandler {
 		BasicDBObject whereQuery = new BasicDBObject();
 		  whereQuery.put("senderId", senderId);
 		  WriteResult cursor3 = chattingIds.remove(whereQuery);
-		  System.out.println("chattingIds in bool"+cursor3.toString());
+		  System.out.println("removeFromChattingBool "+cursor3.toString());
 //		 chattingIdsMap.remove(senderId);
 	}
 	public static void addInChattingBool(String senderId,String recipientId) {
@@ -121,6 +125,13 @@ public class DataHandler {
 	}
 	
 	
+	public static void printall(DBCollection collection){
+		DBCursor cursor = collection.find();
+		  while (cursor.hasNext()) {
+			System.out.println(cursor.next());
+		  }
+
+	}
 	public static void insertDummyDocuments(DBCollection collection) {
 
 		List<DBObject> list = new ArrayList<DBObject>();
@@ -146,107 +157,110 @@ public class DataHandler {
 	}
 
 	public static void main(String[] args) {
-
-	try {
-
-
-	  // get a single collection
-	  DBCollection collection = db.getCollection("dummyColl");
-
-	  insertDummyDocuments(collection);
-
-	  System.out.println("1. Find first matched document");
-	  DBObject dbObject = collection.findOne();
-	  System.out.println(dbObject);
-
-	  System.out.println("\n1. Find all matched documents");
-	  DBCursor cursor = collection.find();
-	  while (cursor.hasNext()) {
-		System.out.println(cursor.next());
-	  }
-
-	  System.out.println("\n1. Get 'name' field only");
-	  BasicDBObject allQuery = new BasicDBObject();
-	  BasicDBObject fields = new BasicDBObject();
-	  fields.put("name", 1);
-
-	  DBCursor cursor2 = collection.find(allQuery, fields);
-	  while (cursor2.hasNext()) {
-		System.out.println(cursor2.next());
-	  }
-
-	  System.out.println("\n2. Find where number = 5");
-	  BasicDBObject whereQuery = new BasicDBObject();
-	  whereQuery.put("number", 5);
-	  DBCursor cursor3 = collection.find(whereQuery);
-	  while (cursor3.hasNext()) {
-			System.out.println("hi");
-			System.out.println(cursor3.next().get("name"));
-	  }
-
-	  System.out.println("\n2. Find where number in 2,4 and 5");
-	  BasicDBObject inQuery = new BasicDBObject();
-	  List<Integer> list = new ArrayList<Integer>();
-	  list.add(2);
-	  list.add(4);
-	  list.add(5);
-	  inQuery.put("number", new BasicDBObject("$in", list));
-	  DBCursor cursor4 = collection.find(inQuery);
-	  while (cursor4.hasNext()) {
-		System.out.println(cursor4.next());
-	  }
-
-	  System.out.println("\n2. Find where 5 > number > 2");
-	  BasicDBObject gtQuery = new BasicDBObject();
-	  gtQuery.put("number", new BasicDBObject("$gt", 2).append("$lt", 5));
-	  DBCursor cursor5 = collection.find(gtQuery);
-	  while (cursor5.hasNext()) {
-		System.out.println(cursor5.next());
-	  }
-
-	  System.out.println("\n2. Find where number != 4");
-	  BasicDBObject neQuery = new BasicDBObject();
-	  neQuery.put("number", new BasicDBObject("$ne", 4));
-	  DBCursor cursor6 = collection.find(neQuery);
-	  while (cursor6.hasNext()) {
-		System.out.println(cursor6.next());
-	  }
-
-	  System.out.println("\n3. Find when number = 2 and name = 'mkyong-2' example");
-	  BasicDBObject andQuery = new BasicDBObject();
-
-	  List<BasicDBObject> obj = new ArrayList<BasicDBObject>();
-	  obj.add(new BasicDBObject("number", 2));
-	  obj.add(new BasicDBObject("name", "mkyong-2"));
-	  andQuery.put("$and", obj);
-
-	  System.out.println(andQuery.toString());
-
-	  DBCursor cursor7 = collection.find(andQuery);
-	  while (cursor7.hasNext()) {
-		System.out.println(cursor7.next());
-	  }
-
-	  System.out.println("\n4. Find where name = 'Mky.*-[1-3]', case sensitive example");
-	  BasicDBObject regexQuery = new BasicDBObject();
-	  regexQuery.put("name",
-		new BasicDBObject("$regex", "Mky.*-[1-3]")
-                    .append("$options", "i"));
-
-	  System.out.println(regexQuery.toString());
-
-	  DBCursor cursor8 = collection.find(regexQuery);
-	  while (cursor8.hasNext()) {
-		System.out.println(cursor8.next());
-	  }
-
-	  collection.drop();
-
-	  System.out.println("Done");
-
-	 } catch (MongoException e) {
-		e.printStackTrace();
-	 }
+		chattingIds.drop();
+		joinedBeforeIds.drop();
+		waitingIds.drop();
+//
+//	try {
+//
+//
+//	  // get a single collection
+//	  DBCollection collection = db.getCollection("dummyColl");
+//
+//	  insertDummyDocuments(collection);
+//
+//	  System.out.println("1. Find first matched document");
+//	  DBObject dbObject = collection.findOne();
+//	  System.out.println(dbObject);
+//
+//	  System.out.println("\n1. Find all matched documents");
+//	  DBCursor cursor = collection.find();
+//	  while (cursor.hasNext()) {
+//		System.out.println(cursor.next());
+//	  }
+//
+//	  System.out.println("\n1. Get 'name' field only");
+//	  BasicDBObject allQuery = new BasicDBObject();
+//	  BasicDBObject fields = new BasicDBObject();
+//	  fields.put("name", 1);
+//
+//	  DBCursor cursor2 = collection.find(allQuery, fields);
+//	  while (cursor2.hasNext()) {
+//		System.out.println(cursor2.next());
+//	  }
+//
+//	  System.out.println("\n2. Find where number = 5");
+//	  BasicDBObject whereQuery = new BasicDBObject();
+//	  whereQuery.put("number", 5);
+//	  DBCursor cursor3 = collection.find(whereQuery);
+//	  while (cursor3.hasNext()) {
+//			System.out.println("hi");
+//			System.out.println(cursor3.next().get("name"));
+//	  }
+//
+//	  System.out.println("\n2. Find where number in 2,4 and 5");
+//	  BasicDBObject inQuery = new BasicDBObject();
+//	  List<Integer> list = new ArrayList<Integer>();
+//	  list.add(2);
+//	  list.add(4);
+//	  list.add(5);
+//	  inQuery.put("number", new BasicDBObject("$in", list));
+//	  DBCursor cursor4 = collection.find(inQuery);
+//	  while (cursor4.hasNext()) {
+//		System.out.println(cursor4.next());
+//	  }
+//
+//	  System.out.println("\n2. Find where 5 > number > 2");
+//	  BasicDBObject gtQuery = new BasicDBObject();
+//	  gtQuery.put("number", new BasicDBObject("$gt", 2).append("$lt", 5));
+//	  DBCursor cursor5 = collection.find(gtQuery);
+//	  while (cursor5.hasNext()) {
+//		System.out.println(cursor5.next());
+//	  }
+//
+//	  System.out.println("\n2. Find where number != 4");
+//	  BasicDBObject neQuery = new BasicDBObject();
+//	  neQuery.put("number", new BasicDBObject("$ne", 4));
+//	  DBCursor cursor6 = collection.find(neQuery);
+//	  while (cursor6.hasNext()) {
+//		System.out.println(cursor6.next());
+//	  }
+//
+//	  System.out.println("\n3. Find when number = 2 and name = 'mkyong-2' example");
+//	  BasicDBObject andQuery = new BasicDBObject();
+//
+//	  List<BasicDBObject> obj = new ArrayList<BasicDBObject>();
+//	  obj.add(new BasicDBObject("number", 2));
+//	  obj.add(new BasicDBObject("name", "mkyong-2"));
+//	  andQuery.put("$and", obj);
+//
+//	  System.out.println(andQuery.toString());
+//
+//	  DBCursor cursor7 = collection.find(andQuery);
+//	  while (cursor7.hasNext()) {
+//		System.out.println(cursor7.next());
+//	  }
+//
+//	  System.out.println("\n4. Find where name = 'Mky.*-[1-3]', case sensitive example");
+//	  BasicDBObject regexQuery = new BasicDBObject();
+//	  regexQuery.put("name",
+//		new BasicDBObject("$regex", "Mky.*-[1-3]")
+//                    .append("$options", "i"));
+//
+//	  System.out.println(regexQuery.toString());
+//
+//	  DBCursor cursor8 = collection.find(regexQuery);
+//	  while (cursor8.hasNext()) {
+//		System.out.println(cursor8.next());
+//	  }
+//
+//	  collection.drop();
+//
+//	  System.out.println("Done");
+//
+//	 } catch (MongoException e) {
+//		e.printStackTrace();
+//	 }
 
 	}
 }
