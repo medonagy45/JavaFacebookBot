e972ba6517c487ac89358ecf5d564400e8741a19
mohamed nagy <mohamed nagy>
2017-05-06 20:20:17 +0200

    Ready to deploy

--- src/com/crunchify/tutorials/Constants.java
+++ src/com/crunchify/tutorials/Constants.java
@@ -3,14 +3,15 @@ package com.crunchify.tutorials;
 import org.json.JSONObject;
 
 public class Constants {
-	public static final String INSTRUCTIONS=JSONObject.quote("Type \"disconnect\" to end chatting, Type \"reconnect\" to reconnect with a new user");
-	public static final String ONCE_FIND_SOMEONE=JSONObject.quote("once we will find you a match we will notify you");
+	public static final String RECONNECT=JSONObject.quote("reconnect");
 	public static final String DISCONNECT_ME=JSONObject.quote("disconnect");
+	public static final String INSTRUCTIONS=JSONObject.quote("Type "+DISCONNECT_ME+" to end chatting, Type "+RECONNECT+" to reconnect with a new user");
+	public static final String ONCE_FIND_SOMEONE=JSONObject.quote("once we will find you a match we will notify you");
 	public static final String YOU_ARE_DISCONNECTED=JSONObject.quote("you are disconnected,");
 	public static final String SAY_HI_TO_RECONNECT=JSONObject.quote("say hi to reconnect");
 	
 	public static final String YOUR_PARTNER_LEFT=JSONObject.quote("your partner left,");
 	public static final String YOU_ARE_CONNECTED=JSONObject.quote("you are now connected ....");
-	public static final String RECONNECT=JSONObject.quote("reconnect");
+	
 	
 }
